7d192e0cb6fb4c8334f97d3ebde39cdd7614d1e9
单浩辉 <1203794209@qq.com>
2020-07-04 13:25:22 +0800

    reducer

--- src/pages/UseContext/components/ReduxContext/index.tsx
+++ src/pages/UseContext/components/ReduxContext/index.tsx
@@ -1,12 +1,16 @@
 import React, {useContext, useReducer} from 'react'
 import {ColorContext, reducer, UPDATE_COLOR} from './reduxContext'
 
-const Show: React.FC = () => {
+interface IShowProps {
+  text: string;
+}
+const Show: React.FC<IShowProps> = (props) => {
   const { state, dispatch } = useContext(ColorContext)
 
   return (
     <div style={ { color: state.color } }>
       当前字体颜色为: {state.color}
+      <div>我是{props.text}</div>
       <button type='button' onClick={ (): void=> dispatch && dispatch({type: UPDATE_COLOR, color: 'red'}) }>红色</button>
       <button type='button' onClick={ (): void=> dispatch && dispatch({type: UPDATE_COLOR, color: 'green'}) }>绿色</button>
     </div>
@@ -19,9 +23,10 @@ const Example: React.FC = () => {
   return (
     <ColorContext.Provider value={ {state, dispatch} }>
       <div>
-        <Show />
+        <div>父组件</div>
         <button type='button' onClick={ (): void=> dispatch && dispatch({type: UPDATE_COLOR, color: 'blue'}) }>蓝色</button>
         <button type='button' onClick={ (): void=> dispatch && dispatch({type: UPDATE_COLOR, color: 'lightblue'}) }>轻绿色</button>
+        <Show text='子组件'/>
       </div>
     </ColorContext.Provider>
   )
